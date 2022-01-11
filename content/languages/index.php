9196987230776f0791fa6fb00618a78c036abc3f
mrghozzi <zoubairghozzi@gmail.com>
2022-01-11 18:05:37 +0100

    Add files via upload

--- content/languages/index.php
+++ content/languages/index.php
@@ -1,11 +1,11 @@
 ﻿<?php
 #####################################################################
 ##                                                                 ##
-##                        My ads v1.2.x                            ##
-##                 http://www.kariya-host.com                      ##
-##                 e-mail: admin@kariya-host.com                   ##
+##                        My ads v2.x.x                            ##
+##                     http://www.krhost.ga                        ##
+##                   e-mail: admin@krhost.ga                       ##
 ##                                                                 ##
-##                       copyright (c) 2018                        ##
+##                       copyright (c) 2019                        ##
 ##                                                                 ##
 ##                    This script is freeware                      ##
 ##                                                                 ##
