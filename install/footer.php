aedf62fc2b712c12f289893b17397e3cabc3783e
mrghozzi <zoubairghozzi@gmail.com>
2022-01-14 22:04:56 +0100

    تعديل طفيف

--- install/footer.php
+++ install/footer.php
@@ -1,5 +1,7 @@
-<div class="footer">
-		<p>Copyright © 2021 All Rights Reserved <a href="http://www.krhost.ga">kariya-host</a></p>
+<?php
+echo "<div class=\"footer\">
+		<p>Copyright © 2022 All Rights Reserved <a href=\"http://www.krhost.ga\">kariya-host</a></p>
 	</div>
 </body>
-</html>
+</html>";
+?>
--- install/header.php
+++ install/header.php
@@ -1,21 +1,22 @@
-<!DOCTYPE html>
+<?php
+echo "<!DOCTYPE html>
 <html>
 <head>
 	<title>Install :: MyAds</title>
-	<link href="css/style.css" rel='stylesheet' type='text/css'/>
+	<link href=\"css/style.css\" rel='stylesheet' type='text/css'/>
 	<link href='//fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' type='text/css'>
 	<link href='//fonts.googleapis.com/css?family=Acme' rel='stylesheet' type='text/css'>
     <!-- For-Mobile-Apps-and-Meta-Tags -->
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />
+		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
+		<script type=\"application/x-javascript\"> addEventListener(\"load\", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 	<!-- //For-Mobile-Apps-and-Meta-Tags -->
-    <script src="js/jquery.min.js"></script>
-	<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
-				<script type="text/javascript">
-					$(document).ready(function () {
-						$('#horizontalTab').easyResponsiveTabs({
-							type: 'default', //Types: default, vertical, accordion           
+    <script src=\"js/jquery.min.js\"></script>
+	<script src=\"js/easyResponsiveTabs.js\" type=\"text/javascript\"></script>
+				<script type=\"text/javascript\">
+					\$(document).ready(function () {
+						\$('#horizontalTab').easyResponsiveTabs({
+							type: 'default', //Types: default, vertical, accordion
 							width: 'auto', //auto or any width like 600px
 							fit: true   // 100% fit in a container
 						});
@@ -23,4 +24,5 @@
 				</script>
 </head>
 <body>
-	<img src="images/logo.png" width="140" alt="company logo"  />
+	<img src=\"images/logo.png\" width=\"140\" alt=\"company logo\"  />";
+?>
