116d3132b2a40ffe4f34cdda18bea0f0f74e1035
Zoubair Ghozzi <zoubairghozzi@gmail.com>
2023-02-25 15:28:51 +0100

    fix: wrongly translated dates

--- include/convertTime.php
+++ include/convertTime.php
@@ -2,11 +2,11 @@
 
 #####################################################################
 ##                                                                 ##
-##                        MYads  v3.x.x                            ##
+##                        My ads v3.0.5(+)                         ##
 ##                     http://www.krhost.ga                        ##
 ##                   e-mail: admin@krhost.ga                       ##
 ##                                                                 ##
-##                       copyright (c) 2022                        ##
+##                       copyright (c) 2023                        ##
 ##                                                                 ##
 ##                    This script is freeware                      ##
 ##                                                                 ##
@@ -16,21 +16,23 @@ if($s_st=="buyfgeufb"){
 //   convertTime
 function convertTime($ptime)
 {
+  global $lang;
+
   if(isset($ptime)AND is_numeric($ptime)) {
     $etime = time() - $ptime;
 
     if ($etime < 1)
     {
-        return '  الأن  ';
+        return $lang['now'];
     }
-    $a = array(  2 * 365* 24 * 60 * 60            =>   '  سنوات <br /> '.date("Y-m-d H:i",$ptime),
-365* 24 * 60 * 60            =>   '  سنة <br /> '.date("Y-m-d H:i",$ptime),
+    $a = array(  2 * 365* 24 * 60 * 60            =>   $lang['years'].'<br /> '.date("Y-m-d H:i",$ptime),
+365* 24 * 60 * 60            =>   $lang['year'].'<br /> '.date("Y-m-d H:i",$ptime),
 30 * 24 * 60 * 60            =>  'شهر',
-7 * 24 * 60 * 60            =>  'أسابيع',
-24 * 60 * 60            =>  'أيام',
-                60 * 60                 =>  'ساعة',
-                60                      =>  'دقيقة',
-                1                       =>  'ثانيه'
+7 * 24 * 60 * 60            =>  $lang['weeks'],
+24 * 60 * 60            =>  $lang['days'],
+                60 * 60                 =>  $lang['hour'],
+                60                      =>  $lang['minute'],
+                1                       =>  $lang['second']
                 );
     foreach ($a as $secs => $str)
     {
