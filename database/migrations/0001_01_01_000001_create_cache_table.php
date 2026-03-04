294f0cda71e9136653bfdf1c833afe209f6791a1
Rayen elghozi <zoubairghozzi@gmail.com>
2026-03-04 06:13:00 +0100

    feat: Add system update feature with UI and initial database migrations for core tables.

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,17 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
-            $table->string('key', 191)->primary();
-            $table->mediumText('value');
-            $table->integer('expiration')->index();
-        });
+        if (!Schema::hasTable('cache')) {
+            Schema::create('cache', function (Blueprint $table) {
+                $table->string('key', 191)->primary();
+                $table->mediumText('value');
+                $table->integer('expiration')->index();
+            });
+        }
 
-        Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key', 191)->primary();
-            $table->string('owner');
-            $table->integer('expiration')->index();
-        });
+        if (!Schema::hasTable('cache_locks')) {
+            Schema::create('cache_locks', function (Blueprint $table) {
+                $table->string('key', 191)->primary();
+                $table->string('owner');
+                $table->integer('expiration')->index();
+            });
+        }
     }
 
     /**
