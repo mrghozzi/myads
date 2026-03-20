485658690a2e12b4257bc5e5d7b2af9689743b41
Rayen elghozi <zoubairghozzi@gmail.com>
2026-03-20 02:22:15 +0100

    feat: Add dynamic sidebar and chat widget JavaScript functionality along with RTL CSS support.

--- themes/default/assets/js/sidebar.js
+++ themes/default/assets/js/sidebar.js
@@ -18,11 +18,12 @@ app.querySelector('.content-grid', function (el) {
 
   const updateGridPosition = function (contentGrid) {
     if (window.innerWidth > breakpointWidth) {
-      const chatWidth = sidebar.chat.active ? sidebar.chat.maxWidth : sidebar.chat.minWidth,
+      const isRTL = document.documentElement.getAttribute('dir') === 'rtl',
+            chatWidth = sidebar.chat.active ? sidebar.chat.maxWidth : sidebar.chat.minWidth,
             navigationWidth = sidebar.navigation.active ? sidebar.navigation.maxWidth : sidebar.navigation.minWidth,
             availableWidth = document.body.clientWidth - contentGrid.offsetWidth - chatWidth - navigationWidth,
-            directionMultiplier = app.isRTL() ? -1 : 1,
-            offsetX = directionMultiplier * ((availableWidth / 2) + navigationWidth);
+            absOffsetX = Math.abs((availableWidth / 2) + navigationWidth),
+            offsetX = isRTL ? -absOffsetX : absOffsetX;
 
       contentGrid.style.transform = `translate(${offsetX}px, 0)`;
     } else {
