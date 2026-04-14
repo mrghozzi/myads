66198febcfa9ac1981e632ded57685c30e1bf3ba
Rayen elghozi <zoubairghozzi@gmail.com>
2026-04-14 19:27:13 +0100

    refactor: remove unused legacy authentication module

--- scratch/find_word.php
+++
@@ -1,8 +0,0 @@
-<?php
-$content = file_get_contents('e:/xampp/htdocs/myads/lang/ar/messages.php');
-$lines = explode("\n", $content);
-foreach ($lines as $i => $line) {
-    if (strpos($line, 'يعمل') !== false) {
-        echo ($i + 1) . ": " . trim($line) . "\n";
-    }
-}
